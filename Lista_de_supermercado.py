0be2062a9c82542065baf0461a1b6acafb042685
DaviMottaa <84351196+DaviMottaa@users.noreply.github.com>
2021-06-01 17:09:57 -0300

    Add files via upload

---
+++ Lista_de_supermercado.py
@@ -0,0 +1,12 @@
+Mercado = ["Maçã","Tangerina","Banana","Manga" ]
+print(Mercado)
+print(Mercado[2].upper()+"!")
+print(Mercado[-2])
+Mercado[3]="Uva"
+print(Mercado)
+Mercado.append("Manga")
+print(Mercado)
+Mercado.insert(2, "Morango")
+print(Mercado)
+Mercado.insert(1, "Goiaba")
+print(Mercado)
